4ba01c42044ea261a8df535ecf7a6ef2ed5de2af
gerasimov707 <gerasimov707@yandex.ru>
2022-07-24 19:34:49 +0300

    коммит

---
+++ ApplicationTests.java
@@ -0,0 +1,13 @@
+package compass.homework_3_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
